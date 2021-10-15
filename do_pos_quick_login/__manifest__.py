d2ac7ede534ce57a86848d3a07e028f4302394fd
120291-Anerol <73196993+120291-Anerol@users.noreply.github.com>
2021-10-14 22:14:46 -0500

    Add files via upload

---
+++ do_pos_quick_login/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    "name" : "POS Logueo directo",
+    "author" : "Sinergia",
+    "email": 'alejandro.avila@sinergia-e.com',
+    "category": "Point of Sale",
+    "summary": "Entra directo en el POS.",
+    "description": """
+    Este modulo envia al usuario al POS directamente
+    """,    
+    'version': '14.1.1.1',
+    "sequence": 1,     
+    "license": 'OPL-1',
+    "depends" : ['base','point_of_sale'],
+    "data" : ['views/res_users_view.xml',
+            ], 
+    "live_test_url" : "",
+    'qweb': [
+        ],
+    "images": ['banner.png'],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    "price": 0,
+    "currency": "MXN"   
+}
+
+
